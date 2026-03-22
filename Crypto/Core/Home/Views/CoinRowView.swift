b204902e2e6f54d5c3339a04dd37d0103b1cabb4
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-03-22 05:10:10 +0200

    Added the cell view for the coins list and added some double extensions

---
+++ Crypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 22/03/2026.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
---
+++ Crypto/Extensions/Double.swift
@@ -0,0 +1,8 @@
+//
+//  Double.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 22/03/2026.
+//
+
+import Foundation
