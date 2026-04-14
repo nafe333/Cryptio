1d7a83bf8dee6b41a5ae6b3ddad21d0588d21a94
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-04-14 06:33:57 +0200

    updated details view to have points chartView using charts , modified the about me section in settings

---
+++ Crypto/Core/DetailView/Views/PointChartView.swift
@@ -0,0 +1,26 @@
+//
+//  CandleChartView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 14/04/2026.
+//
+
+import SwiftUI
+import Charts
+
+struct CandleChartView: View {
+    
+    init(coin: CoinModel){
+        
+    }
+    
+    
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CandleChartView(coin: DeveloperPreview.instance.coin)
+}
